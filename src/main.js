5b2d513aff9d95475d8031e85f8ad2d8cb023868
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2023-11-01 12:57:53 +0100

    Add calculator new project

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if ('+-*/%'.includes(key)) {
             // If an operation key is pressed, call the function to handle operator input
             handleOperatorInput(key);
-        } else if (key === 'Enter') {
+        } else if (key === 'Enter' || key === '=') {
             // If the "Enter" key is pressed, call the function to calculate the result
             calculateResult();
         } else if (key === 'Backspace') {
@@ -31,36 +31,47 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.classList.toggle('dark-theme');
     });
 
+    // Get references to the display and buttons
     const display = document.getElementById('calculator-display');
     const buttons = document.getElementById('calculator-buttons');
 
+    // Initialize variables to store input and display values
     let currentInput = '';
     let displayValue = '';
 
+    // Add a click event listener to the calculator buttons
     buttons.addEventListener('click', function (event) {
+        // Get the button that was clicked and its text content
         const button = event.target;
         const buttonValue = button.textContent;
 
         if (buttonValue === 'AC') {
+            // If the "AC" button is clicked, clear the calculator
             clearCalculator();
         } else if (buttonValue === 'DEL') {
+            // If the "DEL" button is clicked, delete the last digit
             deleteLastDigit();
         } else if (buttonValue === '=') {
+            // If the "=" button is clicked, calculate the result
             calculateResult();
         } else if (buttonValue.match(/[0-9.]/)) {
+            // If a numeric digit or a decimal point is clicked, handle digit input
             handleDigitInput(buttonValue);
         } else if ('+-*/%'.includes(buttonValue)) {
+            // If an operator button is clicked, handle operator input
             handleOperatorInput(buttonValue);
         }
     });
 
     function clearCalculator() {
+        // Reset the input and display values to clear the calculator
         currentInput = '';
         displayValue = '';
         updateDisplay('0');
     }
 
     function deleteLastDigit() {
+        // Remove the last character from the current input and update the display
         if (currentInput.length > 0) {
             currentInput = currentInput.slice(0, -1);
             displayValue = currentInput;
@@ -70,7 +81,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function calculateResult() {
         try {
-            // Replace only the last "%" with "/100*" and then evaluate
+            // Replace only the last "%" with "/100*" and then evaluate,
+            // /(?!.*%.*%.*$)%/  This is a regular expression pattern
             currentInput = eval(
                 currentInput.replace(/(?!.*%.*%.*$)%/, '/100*')
             ).toString();
